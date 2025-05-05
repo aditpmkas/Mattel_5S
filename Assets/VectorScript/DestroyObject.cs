4d9a89b780fee79fdcd6a2d6e934e598251518a6
bisulpetjah <aditya.pamungkas.sdn@gmail.com>
2025-05-05 14:44:38 +0700

    sorting scoring debuglog

--- Assets/VectorScript/DestroyObject.cs
+++ Assets/VectorScript/DestroyObject.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class DestroyObject : MonoBehaviour
 {
+
+    int score = 0;  
+
     private void OnTriggerEnter(Collider other)
     {
 
-        if(gameObject.tag == "TrashCan")
-        {
-            Destroy(other.gameObject);
-            Debug.Log("GameObject " + other.gameObject.name + " destroyed.");
-        }
+
+            if (other.gameObject.tag == "Sort")
+            {
+                score++;
+                Destroy(other.gameObject);
+                Debug.Log("GameObject " + other.gameObject.name + " destroyed.");
+                Debug.Log("Your score is: " + score);
+            }
+            if(other.gameObject.tag == "Unsort")
+            {
+                score--;
+                Destroy(other.gameObject);
+                Debug.Log("GameObject " + other.gameObject.name + " destroyed.");
+                Debug.Log("Your score is: " + score);
+            }
+        
         // Menghancurkan gameObject ini saat menyentuh trigger collider
         
     }
