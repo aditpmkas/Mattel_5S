68850a4629eaa749e04aeb3b9261b9024feea8e6
bisulpetjah <aditya.pamungkas.sdn@gmail.com>
2025-05-13 17:25:46 +0700

    update ui

--- Assets/Script L1/SceneTransition.cs
+++ Assets/Script L1/SceneTransition.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    public void GoToNextScene()
+    public void GoToLevel2()
     {
         SceneManager.LoadScene("L2 M1");
+    }
+    public void GoToLevel1()
+    {
+        SceneManager.LoadScene("L1 M1");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GoToTutorial()
+    {
+        SceneManager.LoadScene("T M1");
     }
 } 
