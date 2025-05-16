deabc57d96136fd875b245557590acc09ff76836
bisulpetjah <aditya.pamungkas.sdn@gmail.com>
2025-05-16 09:56:03 +0700

    update sfx fadel

--- Assets/Script Tutorial/MopTutorial.cs
+++ Assets/Script Tutorial/MopTutorial.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class MopTutorial : MonoBehaviour
 {
+    public AudioSource mopSound; // Assign via Inspector
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("DirtyFloor") || other.CompareTag("DirtyFloor"))
         {
-            PuddleHealthT puddleHealth = other.GetComponent<PuddleHealthT>();
+            // Play sound
+            if (mopSound != null && !mopSound.isPlaying)
+            {
+                mopSound.Play();
+            }
 
+            // Apply mop effect
+            PuddleHealthT puddleHealth = other.GetComponent<PuddleHealthT>();
             if (puddleHealth != null)
             {
                 puddleHealth.AddSwipe();
